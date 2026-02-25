8cb006935c46a6cf88e012d2d6ecaff000c9adc2
khaliduddinn <130018045+khaliduddinn@users.noreply.github.com>
2026-02-25 13:21:21 +0000

    Week 1: setup hello typescript (node 20 LTS)

---
+++ week-01/hello-typescript/index.ts
@@ -0,0 +1,9 @@
+console.log("Hello, TypeScript!");
+
+const age: number = 25;
+const name: string = "Future Koala Engineer";
+const isLearning: boolean = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is learning TypeScript:", isLearning);
